c0346bdf4601a32231bd86a452659fd124a796db
KMoussa33 <kelsey.moussa@gmail.com>
2023-08-04 17:41:13 -0400

    practicing with C++

---
+++ myfirstprogram.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World! \n\n";
+//   cout << "I am learning C++";
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   cout << "Hello World!" << endl;
+//   cout << "I am learning C++";
+//   return 0;
+// }
+
+// #include <iostream>
+
+// int main() {
+//   std::cout << "Hello World!";
+//   return 0;
+// }
+
+
+/* int myNum = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false) */
+
+// Combining text and variables 
+// int myAge = 35;
+// cout << "I am " << myAge << " years old.";
